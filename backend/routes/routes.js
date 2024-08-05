4774836c3665ea884f000212f1750b46e4f94367
raviitaliya <italiyaravi704@gmail.com>
2024-08-05 14:14:31 +0530

    add ts

--- backend/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import auth from '../auth/auth.js';  // Make sure this path is correct
-
-const router = express.Router();
-
-router.use("/auth", auth);  // Assuming you want to use auth routes under /auth path
-
-export default router;
